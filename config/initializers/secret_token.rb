c2569443ed521c72130cdecd39373f0d879ecd17
Sam DeBrule <sam.debrule@gmail.com>
2014-08-01 20:42:15 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bodyweight::Application.config.secret_key_base = 'b8c5a16a3930065e0706c40178d03f92cc2489fdb237bd763ce9fa7c148ab36b2d2c8cdf2abc87fdf3f98aa4b2fcefc7c811c673e37e55b3bfd965414e9c1d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bodyweight::Application.config.session_store :cookie_store, key: '_bodyweight_session'
